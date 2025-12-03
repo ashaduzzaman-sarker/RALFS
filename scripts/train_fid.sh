5244b1d39bb7d87dddea1b874b91ca1bd2469b36
Ashaduzzaman Sarker <ashaduzzaman2505@gmail.com>
2025-12-03 21:16:11 +0600

    Update training script for arXiv-summarization

--- scripts/train_fid.sh
+++ scripts/train_fid.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
-echo "Starting FiD Training on GovReport"
+
+echo "Starting FiD Training on arXiv-summarization (T4 SAFE MODE)"
 
 accelerate launch \
-    --mixed_precision=bf16 \
-    --num_processes=8 \
-    --main_process_port=29501 \
+    --mixed_precision=fp16 \
+    --num_processes=1 \
     src/training/train_fid.py \
-    --config configs/train/fid_train.yaml
+    model.batch_size=1 \
+    model.grad_accum=16 \
+    model.lr=5e-5 \
+    model.epochs=1 \
+    model.name="google/flan-t5-large" \
+    output_dir="checkpoints/arxiv_fid_final"
