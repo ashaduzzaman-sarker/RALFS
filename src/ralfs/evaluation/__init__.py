d67f9f4a0d3ca94ddb1f6d753f37901034b6cf11
Ashaduzzaman Sarker <ashaduzzaman2505@gmail.com>
2025-12-13 03:14:26 +0600

    Initialize evaluation module with imports and exports

---
+++ src/ralfs/evaluation/__init__.py
@@ -0,0 +1,11 @@
+# src/ralfs/training/__init__.py
+from .faithfulness import compute_egf
+from .human import create_human_eval_template
+from .metrics import evaluate_predictions
+from .main import evaluate
+__all__ = [
+    "compute_egf",
+    "create_human_eval_template",
+    "evaluate",
+    "evaluate_predictions"
+]
