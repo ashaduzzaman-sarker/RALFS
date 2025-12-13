f3049da3513f105d94d8caf871e20380e90d44e6
Ashaduzzaman Sarker <ashaduzzaman2505@gmail.com>
2025-12-14 00:53:16 +0600

    Comment out all content in __init__.py

--- src/ralfs/__init__.py
+++ src/ralfs/__init__.py
@@ -1,20 +1,21 @@
-# src/ralfs/__init__.py
-from .core import get_logger, RALFSConfig
-from .data import run_preprocessing, build_index
-from .retriever import create_retriever
-from .generator import create_generator
-from .training import train, FiDDataset
-from .evaluation import evaluate
+# # src/ralfs/__init__.py
+# from .core import get_logger, RALFSConfig
+# from .data import run_preprocessing, build_index
+# from .retriever import create_retriever
+# from .generator import create_generator
+# from .training import train, FiDDataset
+# from .evaluation import evaluate
 
-__version__ = "1.0.0"
-__all__ = [
-    "get_logger",
-    "RALFSConfig",
-    "run_preprocessing",
-    "build_index",
-    "create_retriever",
-    "create_generator",
-    "train",
-    "evaluate",
-]
+# __version__ = "1.0.0"
+# __all__ = [
+#     "get_logger",
+#     "RALFSConfig",
+#     "run_preprocessing",
+#     "build_index",
+#     "create_retriever",
+#     "create_generator",
+#     "train",
+#     "evaluate",
+# ]
+
 
